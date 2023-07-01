b107b177b6173acb33c3727118b65ba78f21a75c
Tamira2804 <tamira2804@gmail.com>
2023-07-01 22:15:28 +0200

    https-changes

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+axios.defaults.baseURL = 'https://';
+
 export async function fetchImages(search, page) {
   const API_KEY = '37970704-9a867c05051ad7b7bfc1fb55a';
   const URL = `https://pixabay.com/api/?key=${API_KEY}&q=${search}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`;
