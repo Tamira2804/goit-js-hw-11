f41b961a9706e95df08d4246406ca6bf7c9cc1f8
Tamira2804 <tamira2804@gmail.com>
2023-07-01 21:51:30 +0200

    axios-changes

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+axios.defaults.baseURL = 'https://pixabay.com/api';
+
 export async function fetchImages(search, page) {
   const API_KEY = '37970704-9a867c05051ad7b7bfc1fb55a';
-  const URL = `https://pixabay.com/api?q=${search}&key=${API_KEY}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`;
+  const URL = `${axios.defaults.baseURL}?key=${API_KEY}&q=${search}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`;
 
   try {
     const response = await axios.get(URL);
